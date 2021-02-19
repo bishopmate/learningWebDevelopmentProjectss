7fb502a6e21ef9e74c229748c54f988b048c5d91
Sashibhushan rajput <sam556913@gmail.com>
2021-02-19 17:58:15 +0530

    Enabling Quantity change  by changingstate

--- cart-mini-project/src/CartItem.js
+++ cart-mini-project/src/CartItem.js
@@ -10,6 +10,29 @@ class CartItem extends React.Component{
       quantity : 1,
       img : ''
     }
+    // this.increaseQuantity = this.increaseQuantity.bind(this);
+  }
+
+  increaseQuantity = () => {
+    console.log(this.state);
+    // setState form 1
+    // this.setState({ title : "Some New Title" });
+    // setState form 2 - if previous state is required then use form 2
+    this.setState((previousState) => {
+      return {
+        quantity : previousState.quantity+1
+      }
+    });
+  }
+
+  decreaseQuantity = () => {
+    if(this.state.quantity >= 1){
+      this.setState( (previousState) => {
+        return{
+          quantity : previousState.quantity-1
+        }
+      });
+    }
   }
 
   render(){
@@ -25,9 +48,22 @@ class CartItem extends React.Component{
           <div style = {{ color : "#777"}}> Quantity :  {quantity}</div>
           <div className = "cart-item-actions">
             {/* Buttons*/}
-            <img alt = "increase" className = "action-icons" src = "https://www.flaticon.com/svg/vstatic/svg/748/748113.svg?token=exp=1613716588~hmac=12e74df51b96b01d251fca4bb7db7481" />
-            <img alt = "decrease" className = "action-icons" src = "https://www.flaticon.com/svg/vstatic/svg/860/860821.svg?token=exp=1613716633~hmac=dfd93c213b7046acdff4a56c63f05704" />
-            <img alt = "delete" className = "action-icons" src = "https://www.flaticon.com/svg/vstatic/svg/833/833262.svg?token=exp=1613718140~hmac=4b9b2ce5417d3866ea27a330ea90423a"/>
+            <img 
+            alt = "increase"
+            className = "action-icons"
+            src = "https://www.flaticon.com/svg/vstatic/svg/748/748113.svg?token=exp=1613716588~hmac=12e74df51b96b01d251fca4bb7db7481"
+            onClick = { this.increaseQuantity } 
+            />
+            <img 
+            alt = "decrease" 
+            className = "action-icons" 
+            src = "https://www.flaticon.com/svg/vstatic/svg/860/860821.svg?token=exp=1613716633~hmac=dfd93c213b7046acdff4a56c63f05704"
+            onClick = { this. decreaseQuantity }
+            />
+            <img 
+            alt = "delete" 
+            className = "action-icons" 
+            src = "https://www.flaticon.com/svg/vstatic/svg/833/833262.svg?token=exp=1613718140~hmac=4b9b2ce5417d3866ea27a330ea90423a"/>
           </div>          
         </div>
       </div>
