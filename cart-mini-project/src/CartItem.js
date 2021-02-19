77dd2e1a06d4ad672c857aa7bc0637a10438ef8d
Sashibhushan rajput <sam556913@gmail.com>
2021-02-19 12:35:16 +0530

    Added state to CartItem Component

--- cart-mini-project/src/CartItem.js
+++ cart-mini-project/src/CartItem.js
@@ -2,18 +2,32 @@ import React from 'react';
 
 class CartItem extends React.Component{
   
+  constructor(){
+    super();
+    this.state = {
+      price : 999,
+      title : 'Mobile Phone',
+      quantity : 1,
+      img : ''
+    }
+  }
+
   render(){
+    const { price, title, quantity } = this.state;
     return (
       <div className = "cart-item">
         <div className = "left-block">
           <img style = {styles.image}/>
         </div>
         <div className = "right-block">
-          <div style = {{ fontSize : 25}}> Mobile </div>
-          <div style = {{ color : "#777"}}> Rs 999 </div>
-          <div style = {{ color : "#777"}}> Qty. 1</div>
+          <div style = {{ fontSize : 25}}> {title}  </div>
+          <div style = {{ color : "#777"}}> Price : {price} </div>
+          <div style = {{ color : "#777"}}> Quantity :  {quantity}</div>
           <div className = "cart-item-actions">
             {/* Buttons*/}
+            <img alt = "increase" className = "action-icons" src = "https://www.flaticon.com/svg/vstatic/svg/748/748113.svg?token=exp=1613716588~hmac=12e74df51b96b01d251fca4bb7db7481" />
+            <img alt = "decrease" className = "action-icons" src = "https://www.flaticon.com/svg/vstatic/svg/860/860821.svg?token=exp=1613716633~hmac=dfd93c213b7046acdff4a56c63f05704" />
+            <img alt = "delete" className = "action-icons" src = "https://www.flaticon.com/svg/vstatic/svg/833/833262.svg?token=exp=1613718140~hmac=4b9b2ce5417d3866ea27a330ea90423a"/>
           </div>          
         </div>
       </div>
