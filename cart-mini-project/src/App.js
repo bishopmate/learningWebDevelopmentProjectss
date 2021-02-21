7201ef8416afd3ca996213708d64a20b2691b0f3
Sashibhushan rajput <sam556913@gmail.com>
2021-02-21 23:17:18 +0530

    Added Total Price Feature and Images for products

--- cart-mini-project/src/App.js
+++ cart-mini-project/src/App.js
@@ -12,28 +12,28 @@ class App extends React.Component {
           price : 499,
           title : "Watch",
           quantity : 2,
-          img : ""
+          img : "https://images-na.ssl-images-amazon.com/images/I/81%2Bd6eSA0eL._UL1500_.jpg"
         },
         { 
           id : 2,
           price : 1999,
           title : "Mobile Phone",
           quantity : 1,
-          img : ""
+          img : "https://assetscdn1.paytm.com/images/catalog/product/M/MO/MOBAPPLE-IPHONESTRI2736846433CC44/1603979127170_0..jpg?imwidth=320&impolicy=hq"
         },
         { 
           id : 3,
           price : 44599,
           title : "PS5",
           quantity : 1,
-          img : ""
+          img : "https://cdn.mos.cms.futurecdn.net/ao26LJcipVKJxoSdd4UpXc-1024-80.jpg.webp"
         },
         { 
           id : 4,
           price : 79999,
           title : "Laptop",
           quantity : 1,
-          img : ""
+          img : "https://images-na.ssl-images-amazon.com/images/I/71cd41RWSNL._SL1500_.jpg"
         }
       ],
       cartQuantity : 5
@@ -89,6 +89,14 @@ class App extends React.Component {
     });
   } 
 
+  getCartTotal = ()=>{
+    const { products } = this.state;
+    let  total = 0;
+    products.forEach(product => {
+      total += product.quantity*product.price;
+    });
+    return total;
+  }
 
   render(){
     const {cartQuantity} = this.state;
@@ -101,6 +109,7 @@ class App extends React.Component {
           onDecreaseQuantity = {this.handleDecreaseQuantity}
           onDeleteProduct = {this.handleDeleteProduct}
         />
+        <div style = { { fontSize : 20, paddingTop : 30, paddingLeft : 50}}>Total Price : {this.getCartTotal()}</div>
       </div>
     );
   }
--- cart-mini-project/src/CartItem.js
+++ cart-mini-project/src/CartItem.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const CartItem = (props) => {
   
-  const { price, title, quantity } = props.product;
+  const { price, title, quantity , img} = props.product;
   const { product, onIncreaseQuantity, onDecreaseQuantity, onDeleteProduct } = props;
   return (
     <div className = "cart-item">
       <div className = "left-block">
-        <img style = {styles.image}/>
+        <img style = {styles.image} src = {img}/>
       </div>
       <div className = "right-block">
         <div style = {{ fontSize : 25}}> {title}  </div>
