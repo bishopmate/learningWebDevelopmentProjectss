162646904117198f999a9a6da7ff359b76c90f28
Sashibhushan rajput <sam556913@gmail.com>
2021-05-12 17:03:15 +0530

    Created the Home Page and started working with creating new articles

--- markdown-blog/server.js
+++ markdown-blog/server.js
@@ -1,11 +1,26 @@
 const express = require('express')
 const app = express();
+const articleRouter = require('./routes/articles');
 const PORT = 5500;
-app.set('view engine', 'ejs');
 
+
+app.set('view engine', 'ejs');
+app.use('/articles', articleRouter);
 
 app.get('/', (req,res)=>{
-  res.render('index');
+  const articles = [
+    {
+      title : "Test Article",
+      createdAt : new Date().toLocaleDateString(),
+      content : 'Test Content'
+    },
+    {
+      title : "Test Article 2",
+      createdAt : new Date().toLocaleDateString(),
+      content : 'Test Content'
+    },
+  ]
+  res.render('articles/index', { articles });
 })
 
 app.listen(PORT, (req, res)=>{
