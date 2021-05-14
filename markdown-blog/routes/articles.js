af5290945c2b0ba8d6b71342901745dd2e93ddfb
Sashibhushan rajput <sam556913@gmail.com>
2021-05-14 16:38:51 +0530

    Completed the feature of creating new articles

--- markdown-blog/routes/articles.js
+++ markdown-blog/routes/articles.js
@@ -1,12 +1,31 @@
 const express = require('express')
 const router = express.Router();
+const Article = require('../models/aritcles');
 
 router.get('/new', (req, res)=>{
-  res.render('articles/new');
+  res.render('articles/new', { article : new Article() });
 });
 
-router.post('/', (req, res)=>{
-  
+router.get('/:id', async (req, res) => {
+  const article = await Article.findById(req.params.id);
+  if(article == null) res.redirect('/');
+  res.render('articles/show', { article });
+})
+
+router.post('/', async (req, res)=>{
+  console.log(req.body);
+  let article = new Article({
+    title : req.body.title,
+    content : req.body.content,
+    markdown : req.body.markdown,  
+  })
+  try{
+    article = await article.save();
+    res.redirect(`/articles/${article.id}`)  
+  }catch(e){
+    console.log(e);
+    res.render('articles/new', { article });
+  }
 })
 
 module.exports = router;
--- markdown-blog/server.js
+++ markdown-blog/server.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express();
+const mongoose = require('mongoose');
 const articleRouter = require('./routes/articles');
 const PORT = 5500;
 
+mongoose.connect('mongodb://localhost/blog', {
+  useNewUrlParser : true,
+  useUnifiedTopology : true
+});
+
+app.use(express.urlencoded({ extended : false }))
 app.set('view engine', 'ejs');
-app.use('/articles', articleRouter);
 
 app.get('/', (req,res)=>{
   const articles = [
@@ -21,6 +27,8 @@ app.get('/', (req,res)=>{
   ]
   res.render('articles/index', { articles });
 })
+
+app.use('/articles', articleRouter);
 
 app.listen(PORT, (req, res)=>{
   console.log(`Listening at PORT ${PORT} succesfully`);
