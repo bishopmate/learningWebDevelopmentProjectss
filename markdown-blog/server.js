5c8e540f5942b27ced268d5e57d8dc26c24600ce
Sashibhushan rajput <sam556913@gmail.com>
2021-05-17 18:18:06 +0530

    initialising project skeleton

--- markdown-blog/server.js
+++ markdown-blog/server.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express');
 const methodOverride = require('method-override');
 const app = express();
 const Article = require('./models/aritcles');
